b46a06759a454b0e69467a10e90a1df826b3f108
Matt Smith <mrandolph83@gmail.com>
2020-10-20 03:29:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_and_supplies|
     puts "#{season.capitalize}:"
     holiday_and_supplies.each do |holiday, supplies|
-      holiday_name = holiday.to_s.split(":")
+      holiday_name = holiday.to_s.split("_")
       holiday_name.each do |word|
         word.capitalize!
       end
