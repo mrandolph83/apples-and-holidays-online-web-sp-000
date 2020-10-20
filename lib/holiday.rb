36ccba634e44307d20377b5388824c6ac42795a0
Matt Smith <mrandolph83@gmail.com>
2020-10-20 03:16:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |seasons, holidays|
+end
+holiday_hash.capitalize
 end
 
 def all_holidays_with_bbq(holiday_hash)
