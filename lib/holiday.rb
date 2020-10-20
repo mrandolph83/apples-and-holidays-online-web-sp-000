39475768730db92c6afd6817f01238dc523e07f3
Matt Smith <mrandolph83@gmail.com>
2020-10-20 03:36:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,6 +75,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash[:spring].keys
   holiday_hash[:summer].keys
 
 end
