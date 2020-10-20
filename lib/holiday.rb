f4634ac90d293ce4cb77950a4a2d9a2b6c0585ca
Matt Smith <mrandolph83@gmail.com>
2020-10-20 03:46:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  new_array = "[]"
+  new_array = []
   holiday_hash.each do |season, holiday_and_supplies|
     holiday_and_supplies.each do |holiday, supplies|
       if supplies = "BBQ"
